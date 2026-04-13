9adee81ead6a974886bdcd595ce7a43db3dc7c9b
kusdhruvUpennGit <kusdhruv@seas.upenn.edu>
2026-04-13 14:43:09 -0400

    Add: reorganize-string - submission-0

---
+++ Data Structures & Algorithms/reorganize-string/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        count = Counter(s)
+        maxHeap = [(-freq,ch) for ch,freq in count.items()]
+        heapq.heapify(maxHeap)
+        prevCount,prevChar = 0,""
+        result = []
+
+        while maxHeap:
+            freq,ch = heapq.heappop(maxHeap)
+            result.append(ch)
+            #put prevChar back if it still has remaining count
+            if prevCount<0:
+                heapq.heappush(maxHeap,(prevCount,prevChar))
+            # current char used once,so increase freq towards 0
+            freq+=1
+            prevCount,prevChar = freq,ch
+        
+        result = "".join(result)
+        if len(result)==len(s):
+            return result
+        else:
+            return ""
