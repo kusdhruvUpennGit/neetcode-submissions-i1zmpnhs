fc6486f73129400f48721b0c104752134b39c84d
kusdhruvUpennGit <kusdhruv@seas.upenn.edu>
2026-05-27 10:59:51 -0400

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> groups;
+
+        for(string str: strs){
+            string key = str;
+            sort(key.begin(),key.end());
+            groups[key].push_back(str);
+        }
+        vector<vector<string>> result;
+
+        for(auto& entry: groups){
+            result.push_back(entry.second);
+        }
+        return result;
+    }
+};
