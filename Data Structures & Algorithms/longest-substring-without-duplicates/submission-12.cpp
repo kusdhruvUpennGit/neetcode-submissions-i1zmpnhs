a8f9287801cafea18bb1874738257fa639a84a96
kusdhruvUpennGit <kusdhruv@seas.upenn.edu>
2026-05-28 21:15:26 -0400

    Add: longest-substring-without-duplicates - submission-12

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-12.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> lastSeen;
+
+        int left = 0;
+        int maxLen = 0;
+
+        for (int right = 0; right<s.size();right++){
+            char c = s[right];
+
+            if(lastSeen.find(c)!= lastSeen.end()){
+                left = max(left, lastSeen[c]+1);
+            }
+
+            lastSeen[c]=right;
+
+            maxLen = max(maxLen,right-left+1);
+
+        }
+        return maxLen;
+    }
+};
